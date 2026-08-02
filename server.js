9f6d4ba0c224029d0655b231c0ffe580a85e2358
Toqeer Tanveer <toqeertanveer93@gmail.com>
2026-08-02 03:47:02 -0700

    Fix homepage route

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/chat", (req, res) => {
 
